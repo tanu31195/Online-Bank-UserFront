b05c2c89c4c48be821b3739fad52c01f0063e7e3
Tanushka PC <tanu31195@users.noreply.github.com>
2019-09-07 15:55:58 +0530

    Initial commit

---
+++ src/test/java/io/github/tanu31195/UserFront/UserFrontApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.tanu31195.UserFront;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserFrontApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
